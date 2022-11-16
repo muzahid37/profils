2d711297e4944028894237409447431206540294
muzahid37 <muzahidul372709@gmail.com>
2022-11-16 23:55:45 +0600

    degine card

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import Profiles from "./Componants/Profiles";
 function App() {
   return (
     <div className="">
-      {/* <h2 className="text-primary">jkdhf</h2> */}
       <Profiles></Profiles>
     </div>
   );
--- src/Componants/Profile.js
+++ src/Componants/Profile.js
@@ -1,11 +1,21 @@
 import React from "react";
+import {
+  AiFillDelete,
+  AiOutlineEdit,
+  AiOutlineGlobal,
+  AiOutlineHeart,
+  AiOutlineMail,
+  AiTwotoneHeart,
+} from "react-icons/ai";
+import { BsTelephone } from "react-icons/bs";
+import "../Componants/Style.css";
 
 const Profile = ({ profile }) => {
   const { name, email, address, website } = profile;
-  console.log(name);
-  console.log(email);
-  console.log(address.zipcode);
-  console.log(website);
+  //   console.log(name);
+  //   console.log(email);
+  //   console.log(address.zipcode);
+  //   console.log(website);
   //   console.log(profile);
   return (
     <>
@@ -16,11 +26,37 @@ const Profile = ({ profile }) => {
           {/* <img src={image} className='card-img-top' alt="" /> */}
           <div className="card-body">
             <h5 className="card-title">{name}</h5>
-            <p className="card-text text-dark item-description">{email}</p>
             <p className="card-text text-dark item-description">
+              <span>
+                <AiOutlineMail />
+              </span>
+              {email}
+            </p>
+            <p className="card-text text-dark item-description">
+              <span>
+                <BsTelephone />
+              </span>
               {address.zipcode}
             </p>
-            <p className="card-text text-dark item-description">{website}</p>
+            <p className="card-text text-dark item-description">
+              <span>
+                <AiOutlineGlobal />
+              </span>
+              {website}
+            </p>
+          </div>
+          <div className="CardHandelar">
+            <ul>
+              <li>
+                <AiOutlineHeart />
+              </li>
+              <li>
+                <AiOutlineEdit />
+              </li>
+              <li>
+                <AiFillDelete />
+              </li>
+            </ul>
           </div>
         </div>
       </div>
